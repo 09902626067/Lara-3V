5d33ff76f74cee5a452e757e28000cccfc171dc7
09902626067 <sltanmwmnd5@gmail.com>
2025-09-26 22:33:21 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qcAjALQC#ZPcfKE88u5_n3BYEuMiLSFAP3IqCMxqwETtWj9J-4MA", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "zUBziKqY#WMecqASqgG_BctE702S3hO0e9j_JH9wJnxzOkomkYsA", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
